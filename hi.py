3479285a459cedf9bc9f4b279224cceb53b7b48d
Paul Wright <paulstuartwright1@gmail.com>
2020-06-10 01:26:33 -0300

    INFERNO

--- hi.py
+++ hi.py
@@ -1,9 +1,6 @@
 print('espero que Dessa vez de certo essa porra')
-<<<<<<< HEAD
 #agora você tem que funcionar porraa AAAA
-=======
 #agora você tem que funcionar porraa
->>>>>>> b2ee77c2fddcd7f1c10604237d5bd40750833ae1
 
 
 print('ana trabalhará abaixo desse print')
