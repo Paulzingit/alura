7c1fa76b7b3c0bf3121d40438d9b2719037c4975
Paul Wright <paulstuartwright1@gmail.com>
2020-06-10 01:10:17 -0300

    a

--- hi.py
+++ hi.py
@@ -1,14 +1,6 @@
 print('espero que Dessa vez de certo essa porra')
-#agora você tem que funcionar
+#agora você tem que funcionar porra
 
 
 print('ana trabalhará abaixo desse print')
-<<<<<<< HEAD
 #vamos ver se Funciona
-=======
-
-#vamos ver se funciona essa merda
-
-#curso de docker???
-
->>>>>>> c46df1f34194ea4e6e1b828a741503234c24dc4b
