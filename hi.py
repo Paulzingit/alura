7c8c7d7147345b4d3c95ba8fe77bb244ea39d053
Paul Wright <paulstuartwright1@gmail.com>
2020-06-10 01:17:08 -0300

    AAA

--- hi.py
+++ hi.py
@@ -1,6 +1,6 @@
 print('espero que Dessa vez de certo essa porra')
-#agora você tem que funcionar porraa 
+#agora você tem que funcionar porra
 
 
 print('ana trabalhará abaixo desse print')
-#vamos ver se Funciona
+#vamos ver se Funciona por favor 
